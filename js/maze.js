62a553dfd57ef06924e71e8a03a497ef165b4d13
Luke <leveloneboi@gmail.com>
2020-02-25 15:31:28 -0700

    Tested variables.  Added more CSS and HTML changing styles for names

--- js/maze.js
+++ js/maze.js
@@ -32,30 +32,126 @@ var cancel = setInterval(incrementSeconds, 1000);
 
 var Grid = 
 [
-    [ , , , , ],
-    [ , , , , ],
-    [ , , , , ],
-    [ , , , , ], 
-    [ , , , , ]
+    [0,2,2,2,2],
+    [1,2,2,2,2],
+    [1,1,1,3,1],
+    [2,2,1,2,1], 
+    [2,2,1,1,1],
 ]
 
-var position = Grid[x][y];
-
-var openArea = 1;
-var block = 2;
-var obstacle = 3;
-
-var startPosition = Grid[0][3];
+var player = false;
+var path = false;
+var block = false;
+var obstacle = false;
+var invisObstacle = false;
 
 
-function move(event)
+var x,y;
+x=0;
+y=0;
+var position = "P"+x+"-"+y;
+
+
+/*for (x=0; x <= 4; x++)
 {
-    var r = event.key;
-    if (r == "w")
+    for(y = 0; y <= 4; y += 1)
     {
-        for (count = 0; count < Grid; count++)
-        {
-            Grid[x][y - 1];
+        console.log(Grid[x][y]);
+
+
+        if (Grid[x][y] == 0)
+        {   
+            player == true;
         }
+        else 
+        {
+            player == false;
+        }
+
+        if (Grid[x][y] == 1)
+        {   
+            path == true;
+        }
+        else 
+        {
+            path == false;
+        }
+
+        if (Grid[x][y] == 2)
+        {   
+            block == true;
+        }
+        else 
+        {
+            block == false;
+        }
+
+        if (Grid[x][y] == 3)
+        {   
+            obstacle == true;
+        }
+        else 
+        {
+            obstacle == false;
+        }
+
+        if (Grid[x][y] == 4)
+        {   
+            invisObstacle == true;
+        }
+        else 
+        {
+            invisObstacle == false;
+        }
+
+
+
     }
+}*/
+
+//var position = Grid[x][y];
+
+
+
+
+
+//var startPosition = Grid[0][0];
+
+
+function moveUp(event)
+{
+    var up = event.key;
+    if (up == "w")
+    {
+        Grid[x][y-1] = player;
+        Grid[x][y] = path;
+
+        document.getElementById(position) == path;
+        y-=1;
+        document.getElementById(position) == player;
+        console.log(x);
+        console.log(y);
+
+    }
+}
+
+function moveDown(event)
+{
+    var down = event.key;
+    if (down == "s")
+    {
+        Grid[x][y+1] = player;
+        Grid[x][y] = path;
+console.log(position);
+        document.getElementById(position).name ="path";
+        
+        y+=1;
+        document.getElementById(position).name ="path";
+        console.log(position);
+        
+        console.log(x);
+        console.log(y);
+
+    }
+    
 }
